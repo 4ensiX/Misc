8de52bc5d5b106f5efeb8531b4db1ebd42533a8a
4ensiX=Zarat <40383134+4ensiX@users.noreply.github.com>
2021-06-20 18:24:45 +0900

    同じ要素が消えてしまう問題
    xlist = [x for x in list if x != pivot]で同じ要素消える

--- python3-play/qsort.py
+++ python3-play/qsort.py
@@ -2,12 +2,14 @@ def qsort(list,r):
     if len(list) <= 1:
         return list
     else:
-        if list[0] < list[1]:
-            pivot = list[1]
-        else:
-            pivot = list[0]
-        xlist = [x for x in list if x != pivot]
-        less = qsort([x for x in xlist if x < pivot],r)
+        #if list[0] < list[1]:
+        #    pivot = list[1]
+        #else:
+        #    pivot = list[0]
+        #xlist = [x for x in list if x != pivot]
+        pivot = list[0]
+        xlist = list[1:]
+        less = qsort([x for x in xlist if x <= pivot],r)
         greater = qsort([x for x in xlist if x > pivot],r)
         if r == 0:#nomal mode greater
             return less + [pivot] + greater
